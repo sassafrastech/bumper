792a3f2cec60183952f92c36c1283da3ce8a4978
Tom Smyth <tom@sassafras.coop>
2016-12-02 12:08:08 -0500

    Checked for deploy param

--- bumper.rb
+++ bumper.rb
@@ -3,4 +3,4 @@ require_relative "lib/bumper"
 
 exec("git checkout develop") unless `git rev-parse --abbrev-ref HEAD` == "develop\n"
 commit_and_merge(do_increment(ARGV[0]))
-do_deploy(ARGV[1])
+do_deploy(ARGV[1]) if ARGV[1]
